26beed3130edf0f09d0030157c223462103d3e44
naikvikrant92 <cr9vikrant@gmail.com>
2021-06-15 16:42:02 +0530

    Added code for giveMeSomething problem.

---
+++ EdaBit/Python/ReturnSomething.py
@@ -0,0 +1,11 @@
+def giveMeSomething(stringConcat):
+    stringToConcatenate=stringConcat
+    stringSomething="Something "
+    result=stringSomething+stringToConcatenate
+    print("Result: ",result)
+    return result
+
+giveMeSomething("is better than nothing")
+
+
+
