8375f2e1070b16dfa51fd767eda50af4710b428b
naikvikrant92 <cr9vikrant@gmail.com>
2021-06-19 17:50:33 +0530

    Added code to solve problem for MaximumEdgeOfTriangle.py

---
+++ EdaBit/Python/MaximumEdgeOfTriangle.py
@@ -0,0 +1,8 @@
+def next_edge(side1,side2):
+    third_side_range=(side1+side2)-1
+    print("Maximum range of the third side:",third_side_range)
+    return third_side_range
+
+
+next_edge(8,10)
+next_edge(5,7)
