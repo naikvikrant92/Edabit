ef995d6ee7c5492c1134b0a0e1184021f7d4f65b
naikvikrant92 <cr9vikrant@gmail.com>
2021-06-20 13:16:00 +0530

    Added code to convert HoursToSeconds.py

---
+++ EdaBit/Python/HoursToSeconds.py
@@ -0,0 +1,9 @@
+def how_many_seconds(hours):
+    print("Hours entered: ",hours)
+    print("Hours in seconds: ",hours*60*60)
+    return hours*60
+
+
+how_many_seconds(5)
+
+#E:\repos\EdaBit\Python
